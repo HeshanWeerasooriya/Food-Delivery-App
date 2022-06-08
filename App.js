8f38ceee0bcf69766d3b9a5cf16b91eb29c3e00c
Heshan <heshan.imedha@gmail.com>
2022-06-08 07:06:56 +0530

    Onboarding Screen

--- App.js
+++ App.js
@@ -1,20 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Image, StatusBar } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+import { COLORS, SIZES } from './constants/theme';
+
+
+const slides = [
+  {
+    id: 1,
+    title: 'Discover Best Places',
+    description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"',
+    image: require('./assets/onboardScreen1.png')
+  },
+  {
+    id: 2,
+    title: 'Choose A Tasty Dish',
+    description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"',
+    image: require('./assets/onboardScreen2.png')
+  },
+  {
+    id: 3,
+    title: 'Pick Up The Delivery',
+    description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"',
+    image: require('./assets/onboardScreen3.png')
+  }
+]
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [showHomePage, setShowHomePage] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const buttonLabel = (label) => {
+    return(
+      <View style={{
+        padding: 12
+      }}>
+        <Text style={{
+          color: COLORS.title,
+          fontWeight: '900',
+          fontSize: SIZES.h4,
+        }}>
+          {label}
+        </Text>
+      </View>
+    )
+  }
+
+  if(!showHomePage) {
+    return(
+      <AppIntroSlider
+        data={slides}
+        renderItem={({item}) => {
+          return(
+            <View style={{
+              flex: 1,
+              alignItems: 'center',
+              padding: 15,
+              paddingTop: 100,
+            }}>
+              <Image
+                source={item.image}
+                style={{
+                  width: SIZES.width - 80,
+                  height: 400,
+                }}
+                resizeMode="contain"
+              />
+              <Text style={{
+                fontWeight: 'bold',
+                color: COLORS.title,
+                fontSize: SIZES.h1,
+              }}>
+                {item.title}
+              </Text>
+              <Text style={{
+                textAlign: 'center',
+                paddingTop: 5,
+                color: COLORS.title
+              }}>
+                {item.description}
+              </Text>
+            </View>
+          )
+        }}
+        activeDotStyle={{
+          backgroundColor: COLORS.primary,
+          width: 30,
+        }}
+        showSkipButton
+        renderNextButton={() => buttonLabel("Next")}
+        renderSkipButton={() => buttonLabel("Skip")}
+        renderDoneButton={() => buttonLabel("Done")}
+        onDone={() => {
+          setShowHomePage(true);
+        }}
+      />
+    )
+  }
+
+  return(
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+    >
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
